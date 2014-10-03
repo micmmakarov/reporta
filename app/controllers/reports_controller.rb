b8402f179cba9ce274a104b4244bc081fdb0a52f
Michael Makarov <info1117@gmail.com>
2014-10-02 17:08:16 -0700

    Rails App with Angular and Bootstrap that filters user events data to make reports on QuickBooks: hook up a Github Api to get the events

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,7 @@
 class ReportsController < ApplicationController
   def index
     user = current_user
-    #events = HTTParty.get("https://api.github.com/users/#{user.nickname}/events?access_token=#{user.token}", headers: {"User-Agent" => "MEOW", "Accept" => "application/vnd.github.v3+json"})
-    events = []
+    events = HTTParty.get("https://api.github.com/users/#{user.nickname}/events?access_token=#{user.token}", headers: {"User-Agent" => "MEOW", "Accept" => "application/vnd.github.v3+json"})
     if events.any?
     	user.cached_events = events.to_json
     	user.save!
